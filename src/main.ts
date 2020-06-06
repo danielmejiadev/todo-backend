732321e4e92e91184c6131af5509431f3f0a03d1
Daniel Mejia <luisdanielmejia@outlook.com>
2020-06-06 11:31:23 -0500

    Initialize app
    - Add modules avaiables to app root.

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   buildDocs(app);
   app.enableCors();
-  
+
   const { PORT } = process.env;
-  await app.listen(PORT || 3000);
+  await app.listen(PORT || 5000);
 }
-bootstrap();
+bootstrap(); 
